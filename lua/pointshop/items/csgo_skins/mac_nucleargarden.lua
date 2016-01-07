319de7c939726e51aa2dc5c7ec193dcda15c6a46
ClockTic <clocktic1@gmail.com>
2016-01-07 17:29:45 -0500

    Added Mac10 Skins

---
+++ lua/pointshop/items/csgo_skins/mac_nucleargarden.lua
@@ -0,0 +1,32 @@
+ITEM.Name = "Mac10| Nuclear Garden"
+ITEM.Price = 10000
+ITEM.Model = "models/weapons/tfa_csgo/w_mac10.mdl"
+ITEM.Except = true
+ITEM.DelayedEquip = false
+
+ITEM.SubCategory = "mac10"
+
+ITEM.WeaponEntity = "weapon_ttt_csgo_mac10"
+ITEM.CSGOSkin = "models/tfa_csgo/cringecity_reskinpack_mac10/mac_nucleargarden"
+
+function ITEM:OnEquip(ply, modifications)
+    if not ply.csgoSkins then
+        ply.csgoSkins = {}
+    end
+
+    ply.csgoSkins[self.WeaponEntity] = self.CSGOSkin
+end
+
+function ITEM:OnHolster(ply)
+    if ply.csgoSkins then
+        ply.csgoSkins[self.WeaponEntity] = nil
+    end
+end
+
+-- This /should/ only get called for modifying the preview
+function ITEM:ModifyClientsideModel(ply, ent, pos, ang)
+    if not ent.csgoSkinApplied then
+        ent:SetMaterial(self.CSGOSkin)
+        ent.csgoSkinApplied = true
+    end
+end
