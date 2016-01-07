5c649a60b8dfffe9847226abb50235ca39f48136
ClockTic <clocktic1@gmail.com>
2016-01-06 22:00:50 -0500

    Added Galil Skins

---
+++ lua/pointshop/items/csgo_skins/galil_cerberus.lua
@@ -0,0 +1,32 @@
+ITEM.Name = "Galil | Cerberus"
+ITEM.Price = 10000
+ITEM.Model = "models/weapons/tfa_csgo/w_galil.mdl"
+ITEM.Except = true
+ITEM.DelayedEquip = false
+
+ITEM.SubCategory = "Galil"
+
+ITEM.WeaponEntity = "weapon_ttt_csgo_galil"
+ITEM.CSGOSkin = "models/tfa_csgo/cringecity_reskinpack_galil/galil_cerberus"
+
+function ITEM:OnEquip(ply, modifications)
+    if not ply.csgoSkins then
+        ply.csgoSkins = {}
+    end
+
+    ply.csgoSkins[self.WeaponEntity] = self.CSGOSkin
+end
+
+function ITEM:OnHolster(ply)
+    if ply.csgoSkins then
+        ply.csgoSkins[self.WeaponEntity] = nil
+    end
+end
+
+-- This /should/ only get called for modifying the preview
+function ITEM:ModifyClientsideModel(ply, ent, pos, ang)
+    if not ent.csgoSkinApplied then
+        ent:SetMaterial(self.CSGOSkin)
+        ent.csgoSkinApplied = true
+    end
+end
