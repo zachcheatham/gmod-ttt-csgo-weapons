86b8cf81b135afcf3749e297c80b81e37e965428
ClockTic <clocktic1@gmail.com>
2016-01-07 17:33:49 -0500

    Added weapons to pointshop

--- lua/pointshop/items/csgo_skins/__category.lua
+++ lua/pointshop/items/csgo_skins/__category.lua
@@ -6,5 +6,5 @@ CATEGORY.EnableSubcategories = true
 CATEGORY.AllowedSubCategoryEquipped = 1
 CATEGORY.DefaultSubcategoryName = "M4A4"
 CATEGORY.SubcategoryOrder = {
-    "M4A4",
+    "M4A4", "Deagle", "Five-Seven", "Galil", "M249", "ak47", "m4a1", "glock18", "mac10"
 }
