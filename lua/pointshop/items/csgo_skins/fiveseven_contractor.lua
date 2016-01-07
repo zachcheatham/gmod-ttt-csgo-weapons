7e5c761f473ed8c99232b247d439be721bc9f967
ClockTic <clocktic1@gmail.com>
2016-01-06 21:47:20 -0500

    Added Five-Seven Skins

---
+++ lua/pointshop/items/csgo_skins/fiveseven_contractor.lua
@@ -0,0 +1,32 @@
+ITEM.Name = "Five-Seven | Contractor"
+ITEM.Price = 10000
+ITEM.Model = "models/weapons/tfa_csgo/w_fiveseven.mdl"
+ITEM.Except = true
+ITEM.DelayedEquip = false
+
+ITEM.SubCategory = "Five-Seven"
+
+ITEM.WeaponEntity = "weapon_ttt_csgo_fiveseven"
+ITEM.CSGOSkin = "models/tfa_csgo/cringecity_reskinpack_fiveseven/fiveseven_contractor"
+
+function ITEM:OnEquip(ply, modifications)
+    if not ply.csgoSkins then
+        ply.csgoSkins = {}
+    end
+
+    ply.csgoSkins[self.WeaponEntity] = self.CSGOSkin
+end
+
+function ITEM:OnHolster(ply)
+    if ply.csgoSkins then
+        ply.csgoSkins[self.WeaponEntity] = nil
+    end
+end
+
+-- This /should/ only get called for modifying the preview
+function ITEM:ModifyClientsideModel(ply, ent, pos, ang)
+    if not ent.csgoSkinApplied then
+        ent:SetMaterial(self.CSGOSkin)
+        ent.csgoSkinApplied = true
+    end
+end
