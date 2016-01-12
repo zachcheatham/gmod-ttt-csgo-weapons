49ae15d0c3afaf59bc662ff4e1c00d305b2cccf6
Zach Cheatham <zachcheatham@gmail.com>
2016-01-11 19:12:22 -0600

    Added addons check.

--- lua/autorun/client/csgo_weapon_skins.lua
+++ lua/autorun/client/csgo_weapon_skins.lua
@@ -15,3 +15,30 @@ net.Receive("ClearViewmodelCSGOSkin", function()
         LocalPlayer():GetViewModel():SetMaterial()
     end
 end)
+
+local check_files = {
+    "materials/models/tfa_csgo/cringecity_reskinpack_ak47/ak47_cartel.vmt",
+    "materials/models/tfa_csgo/cringecity_reskinpack_deagle/deagle_blaze.vmt",
+    "materials/models/tfa_csgo/lc_skins/fiveseven/fiveseven_banana.vmt",
+    "materials/models/tfa_csgo/cringecity_reskinpack_galil/galil_aquaterrace.vmt",
+    "materials/models/tfa_csgo/cringecity_reskinpack_glock/glock_bluefissure.vmt",
+    "materials/models/tfa_csgo/cringecity_reskinpack_m249/m249_magma.vmt",
+    "materials/models/tfa_csgo/lc_skins/m4a1/m4a1s_atomicalloy.vmt",
+    "materials/models/tfa_csgo/cringecity_reskinpack_m4a4/m4a4_asiimov.vmt",
+    "materials/models/tfa_csgo/cringecity_reskinpack_mac10/mac_amberfade.vmt"
+}
+
+local function displayCollectionDialog()
+    chat.AddText("It appears you haven't downloaded our optional weapon skins. Type !skins to open the Workshop collection.")
+end
+
+local function playerReady()
+    for _, f in ipairs(check_files) do
+        if not file.Exists(f, "GAME") then
+            print (f)
+            displayCollectionDialog()
+            return
+        end
+    end
+end
+hook.Add("MOTDClosed", "CSGOWeaponSkins", playerReady)
