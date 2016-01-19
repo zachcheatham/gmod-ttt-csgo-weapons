89fd16bc15dcc55ea3e9cfb3271661674d5781aa
Zach Cheatham <zachcheatham@gmail.com>
2016-01-19 13:35:06 -0600

    Refresh equipped weapons' skins after the pointshop has completed player spawn item equips.

--- lua/autorun/server/csgo_weapon_skins.lua
+++ lua/autorun/server/csgo_weapon_skins.lua
@@ -1,1 +1,10 @@
 util.AddNetworkString("CSGOWeaponSkins_UpdateViewModelSkin")
+
+local function postSpawnEquip(ply, delayed)
+    for _, wep in ipairs(ply:GetWeapons()) do
+        if wep.SetCSGOSkin and ply.csgoSkins and ply.csgoSkins[wep:GetClass()] and not wep:HasCSGOSkin() then
+            wep:SetCSGOSkin(ply.csgoSkins[wep:GetClass()])
+        end
+    end
+end
+hook.Add("PS_PostSpawnEquip", "CSGOWeaponSkins_PostSpawnEquip", postSpawnEquip)
