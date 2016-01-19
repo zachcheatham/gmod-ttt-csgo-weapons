565e454319df340993721189d99013539e48a78c
Zach Cheatham <zachcheatham@gmail.com>
2016-01-19 15:20:43 -0600

    Only update player's weapons' skins on after non-delayed items are equipped.

--- lua/autorun/server/csgo_weapon_skins.lua
+++ lua/autorun/server/csgo_weapon_skins.lua
@@ -1,9 +1,11 @@
 util.AddNetworkString("CSGOWeaponSkins_UpdateViewModelSkin")
 
 local function postSpawnEquip(ply, delayed)
-    for _, wep in ipairs(ply:GetWeapons()) do
-        if wep.SetCSGOSkin and ply.csgoSkins and ply.csgoSkins[wep:GetClass()] and not wep:HasCSGOSkin() then
-            wep:SetCSGOSkin(ply.csgoSkins[wep:GetClass()])
+    if not delayed then -- There aren't any delayed CS:GO items
+        for _, wep in ipairs(ply:GetWeapons()) do
+            if wep.SetCSGOSkin and ply.csgoSkins and ply.csgoSkins[wep:GetClass()] and not wep:HasCSGOSkin() then
+                wep:SetCSGOSkin(ply.csgoSkins[wep:GetClass()])
+            end
         end
     end
 end
