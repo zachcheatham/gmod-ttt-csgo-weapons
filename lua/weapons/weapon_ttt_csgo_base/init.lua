bb2491ad0c88cd092799bae5cbbaf62ba9ec1a8f
Zach Cheatham <zachcheatham@gmail.com>
2016-01-19 12:08:01 -0600

    Update reskin check.

--- lua/weapons/weapon_ttt_csgo_base/init.lua
+++ lua/weapons/weapon_ttt_csgo_base/init.lua
@@ -5,7 +5,7 @@ include("shared.lua")
 function SWEP:Equip(newOwner)
     self.BaseClass.BaseClass.Equip(self, newOwner)
 
-    if not self.csgoSkinData then -- Only skin weapons that haven't been skinned yet
+    if not self:GetCSGOSkin() or string.len(self:GetCSGOSkin()) == 0 then -- Only skin weapons that haven't been skinned yet
         if self:GetOwner().csgoSkins then
             local skin = self:GetOwner().csgoSkins[self:GetClass()] -- Check if player has a skin for this weapon
             if skin then
