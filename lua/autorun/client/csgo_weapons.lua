d55a23f60d98c8e269f3db2d8cace4e80153bd1d
Zach Cheatham <zachcheatham@gmail.com>
2016-01-25 13:26:26 -0600

    Only need 1 skin per addon.

--- lua/autorun/client/csgo_weapons.lua
+++ lua/autorun/client/csgo_weapons.lua
@@ -79,8 +79,7 @@ local checkFiles = {
     "materials/models/tfa_csgo/cringecity_reskinpack_mac10/mac_amberfade.vmt",
     "materials/models/tfa_csgo/cringecity_reskinpack_famas/famas_afterimage.vmt",
 	"materials/models/tfa_csgo/cringecity_reskinpack_r8/r8_crimsonweb.vmt",
-	"materials/models/tfa_csgo/clocktic_reskinpack_nova/nova_moon.vmt",
-	"materials/models/tfa_csgo/clocktic_reskinpack_p90/p90_trigon.vmt"
+	"materials/models/tfa_csgo/clocktic_reskinpack_nova/nova_moon.vmt"
 }
 
 local function displayCollectionDialog()
